90f3edd4168f32737106b3ed1caf90685e19e497
Antonenko Ilya <antonenkoilya01@gmail.com>
2016-04-07 16:45:13 +0300

    Python lab2 (External sort). Now program can read buffers, sort it and store sorted data in temporary files.

--- ISP/4term/lab2/task1.py
+++ ISP/4term/lab2/task1.py
@@ -1,7 +1,35 @@
-import tempfile as tm
+import tempfile as tf
+
+
+def sortBuffer(buff):
+    tempfile = tf.TemporaryFile()
+    sortedList = sorted(buff.split('\n'))
+    tempfile.write('\n'.join(sortedList))
+    return tempfile
+
+
+def sort(fileName, bufferSize):
+    with open(fileName, 'r') as unsortedFile:
+        bufferSize -= bufferSize % len(unsortedFile.readline())
+        unsortedFile.seek(0)
+
+        tempFiles = []
+
+        while True:
+            buff = unsortedFile.read(bufferSize)
+            if not buff:
+                break
+            tempFiles.append(sortBuffer(buff))
+
+        print len(tempFiles)
+        tempFiles[0].seek(0)
+        print tempFiles[0].read()
 
 
 def main():
+    unsortedFileName = 'file'
+    bufferSize = 500
+    sort(unsortedFileName, bufferSize)
 
 
 if __name__ == '__main__':
